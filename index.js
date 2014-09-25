72de86a5720fad4f6165869130c6077f9661cc65
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-24 22:43:08 -0400

    working on page flip

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * component.collection-page-flip
+ * http://github.com/typesettin/component.collection-page-flip
+ *
+ * Copyright (c) 2013 AmexPub. All rights reserved.
+ */
+
+'use strict';
+
+module.exports = require('./lib/component.collection-page-flip');
